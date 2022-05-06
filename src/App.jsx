eaf9db9c999a393c390b5a3b209d50f1880bf700
ruddy_flores <ninjaneja@gmail.com>
2022-05-06 10:00:59 -0400

    primer componente

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+import {Rojo } from './Componentes/Rojo.jsx'
+/*import {DarkWork } from './Componentes/DarkWork.jsx'
+import {ColorWork } from './Componentes/ColorWork.jsx'
+import {DarkPlay } from './Componentes/DarkPlay.jsx'
+import {ColorPlay } from './Componentes/ColorPlay.jsx'
+import {DarkStudy } from './Componentes/DarkStudy.jsx'
+import {ColorStudy } from './Componentes/ColorStudy.jsx'
+import {DarkExercise } from './Componentes/DarkExercise.jsx'
+import {ColorExercise } from './Componentes/ColorExercise.jsx'
+import {DarkSocial } from './Componentes/DarkSocial.jsx'
+import {ColorSocial } from './Componentes/ColorSocial.jsx'
+import {DarkSelfCare } from './Componentes/DarkSelfCare.jsx'
+import {ColorSelfCare } from './Componentes/ColorSelfCare.jsx'*/
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Rojo/>
+
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/Componentes/Rojo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Componentes/Rojo.css'
+
+function ColorRojo() {
+  return (
+    <div className='ColorRojo'>
+    
+    </div>
+    
+  )
+}
+export {ColorRojo}
+
